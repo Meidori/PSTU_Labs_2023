bde7b0167dcb006b4881fa2971971b42c98b840e
Meidori <meidorislav@gmail.com>
2024-02-19 12:57:21 +0500

    Code 7_2_5.cpp

---
+++ Sem_2/Labs/7_2_ЗЕЛ/7_2_5.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <stdarg.h>
+using namespace std;
+
+
+int sum(const int k, ...) {
+    va_list args;
+    va_start(args, k);
+    int result = 0;
+    int tmp = 0;
+    int cur_term = 0;
+    int terms[k - 1];
+
+    for (int i = 0; i < k; i++) {
+        tmp = va_arg(args, int);
+        if (i == 0) {
+            cur_term = tmp;
+        }
+        else {
+            cur_term *= tmp;
+            terms[i - 1] = cur_term;
+            cur_term = tmp;
+        }
+    }
+
+    for (int i = 0; i < k - 1; i++) {
+        if (i % 2 == 0) {
+            result += terms[i];
+        }
+        else result -= terms[i];
+    }
+
+    va_end(args);
+    return result;
+}
+
+
+
+
+int main() {
+    cout << sum(5, 1, 2, 3, 4, 5) << endl;
+    cout << sum(10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10) << endl;
+    cout << sum(12, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12) << endl;
+
+
+    return 0;
+}
