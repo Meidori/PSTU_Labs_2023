4840da7346f62418ca6777ccb26f3d79d9a6ffe0
Meidori <meidorislav@gmail.com>
2024-02-25 09:50:20 +0500

    New solution

--- Sem_2/Labs/3/queens/queens.cpp
+++ Sem_2/Labs/3/queens/queens.cpp
@@ -2,103 +2,85 @@
 using namespace std;
 
 
-int board[8][8];
-const int QUEEN = -1;
+int board[8][8] = {0}; 
 
 
-void resetBoard();
-void showBoard();
-bool checkQueen(int i);
 void setQueen(int i, int j);
-void deleteQueen(int i, int j);
+void removeQueen(int i, int j);
+void printPosition();
+void solve(int i);
 
 
 int main() {
-	resetBoard();
-	checkQueen(0);
-	showBoard();
-
-	return 0;
+    solve(0);
 }
 
 
-void resetBoard() {
-	int board[8][8] = {0};
+void setQueen(int i, int j) {
+    for (int x = 0; x < 8; x++) {
+        board[x][j]++;
+        board[i][x]++;
+        if (i + j - x >= 0 && i + j - x < 8) {
+            board[i + j - x][x]++;
+        }
+        if (i - j + x >= 0 && i - j + x < 8) {
+            board[i - j + x][x]++;
+        }
+    }
+
+    board[i][j] = -1;
 }
 
 
-void showBoard() {
-	for (int i = 0; i < 8; i++) {
-		for (int j = 0; j < 8; j++) {
-			if (board[i][j] == QUEEN) {
-				cout << "[Q] ";
-			}
-			else {
-				cout << "[ ] ";
-			}
-		}
-		cout << endl;
-	}
+void removeQueen(int i, int j) {
+    for (int x = 0; x < 8; x++) {
+        board[x][j]--;
+        board[i][x]--;
+        if (i + j - x >= 0 && i + j - x < 8) {
+            board[i + j - x][x]--;
+        }
+        if (i - j + x >= 0 && i - j + x < 8) {
+            board[i - j + x][x]--;
+        }
+    }
+
+    board[i][j] = 0;
 }
 
 
-void setQueen(int x, int j) {
-	int d; 
-	for (int i = 0; i < 8; ++i) { 
-		board[i][j]++;              //указываем на клетку, атакуемую по вертикали
-		board[x][i]++;              //указываем на клетку по горизонтали
-
-		d = j - i + x;              //вычисляем 1 диагональ
-		if (d >= 0 && d < 8) {      
-			board[i][d]++;
-		}
-		d = j + i - x;              //вычисляем 2 диагональ
-
-		if (d >= 0 && d < 8) {
-			board[i][d]++;
-		}
-	}
-	board[x][j] = QUEEN;
-}
-
-
-void deleteQueen(int x, int j) {
-	int d;
-	for (int i = 0; i < 8; ++i) { 
-		board[i][j]--;                  //указываем на клетку, атакуемую по вертикали
-		board[x][i]--;                  //указываем на клетку, атакуемую по горизонтали
-
-		d = j - x + i;                  //вычисляем 1 диагональ
-		if (d >= 0 && d < 8) { 
-			board[i][d]--;
-		}
-		d = j + x - i;                  //вычисляем 2 диагональ
-
-		if (d >= 0 && d < 8) {
-			board[i][d]--;
-		}
-	}
-	board[x][j] = 0;
-}
-
- 
-bool checkQueen(int i) {
-	bool result = false;
-	for (int j = 0; j < 8; ++j) {
-		if (board[i][j] == 0) {
-			setQueen(i, j);         //пробуем поставить ферзя и разметить клетки под боем  
-
-			if (i == 7) { 
-                result = true; 
+void printPosition() {
+    char alphabet[8] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'};
+    char line[8];
+    int column[8];
+    int index = 0;
+    for (int i = 0; i < 8; i++) {
+        for (int j = 0; j < 8; j++) {
+            if (board[i][j] == -1) {
+                line[index] = alphabet[j];
+                column[index] = i + 1;
+                index++;
             }
-			else if (!(result = checkQueen(i + 1))) { 
-                deleteQueen(i, j); 
-            }
-		}
-		if (result) {
-			break;
-		}
-	}
-	return result;  
+        }
+    }
+
+    for (int i = 0; i < 8; i++) {
+        cout << line[i] << column[i] << " ";
+    }
+    cout << endl;
 }
 
+
+void solve(int i) {
+    for (int j = 0; j < 8; j++) {
+        if (board[i][j] == 0) {
+            setQueen(i, j);
+            if (i == 7) {
+                printPosition();
+            }
+            else {
+                solve(i + 1);
+            }
+            removeQueen(i, j);
+        }
+    }
+}
