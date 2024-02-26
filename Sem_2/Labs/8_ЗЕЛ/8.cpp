107d1ad55100b1ef953b1842feb3e0be0539c78d
Meidori <meidorislav@gmail.com>
2024-02-27 00:10:34 +0500

    Code 8.cpp

---
+++ Sem_2/Labs/8_ЗЕЛ/8.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <string>
+#include <stdio.h>
+using namespace std;
+
+
+FILE *f;
+
+
+struct Human {
+    char full_name[100];
+    int year_of_birth;
+    float height;
+    float weight;
+};
+
+
+Human input_Human() {
+    string name;
+    Human obj;
+    getline(cin, name);
+    for (int i = 0; i < name.length(); i++) {
+        obj.full_name[i] = name[i];
+    }
+    cin >> obj.year_of_birth;
+    cin >> obj.height;
+    cin >> obj.weight;
+    return obj;
+}
+
+
+void write(Human user) {
+    fwrite(&user, sizeof(Human), 1, f);
+}
+
+
+void output(Human user) {
+    fread(&user, sizeof(Human), 1, f);
+    printf("%s %d %f %f",user.full_name, user.year_of_birth, user.height, user.weight);
+}
+
+
+int main() {
+    Human user;
+    f = fopen("file.dat", "w+");
+    if (f == NULL) {
+        cout << "Не удалось открыть файл" << endl;
+    }
+    else {
+        cout << "Файл открыт" << endl;
+        cout << "Введите ФИО, год рождения, рост, вес:" << endl;
+        Human user = input_Human();
+        write(user);
+        output(user);
+    }
+}
