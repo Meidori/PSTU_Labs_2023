547869cf83ddf90731bf90ab12799792e887949f
Meidori <meidorislav@gmail.com>
2024-02-27 14:31:37 +0500

    Init new solution 8.cpp

--- Sem_2/Labs/8_ЗЕЛ/8.cpp
+++ Sem_2/Labs/8_ЗЕЛ/8.cpp
@@ -8,15 +8,65 @@ FILE *f;
 
 
 struct Human {
-    char full_name[100];
+    string full_name;
     int year_of_birth;
-    float height;
-    float weight;
+    string height;
+    string weight;
 };
 
 
+void add_notes(Human &user) {
+    ofstream file("file.txt", ios::binary);
+
+    cout << "Введите ФИО, год рождения, рос, вес:" << endl;
+    getline(cin, user.full_name);
+    cin >> user.year_of_birth;
+    cin >> user.height;
+    cin >> user.weight;
+
+    file << user.full_name << "\n";
+    file << user.year_of_birth << "\n";    
+    file << user.height << "\n";    
+    file << user.weight << "\n";    
+    
+    file.close();
+}
+
+
+void show_notes() {
+    ifstream file("file.txt", ios::binary);
+    string line;
+    while (getline(file, line)) {
+        cout << line << endl;
+    }
+
+    file.close();
+}
+
+
+void replace_notes(Human &user) {
+    string filename = "file.txt";
+    string tmpname = "tmp.txt";
+    ifstream file(filename, ios::binary);
+    ofstream tmp(tmpname, ios::binary);   
+        
+
+    file.close();
+    tmp.close();
+
+    remove(filename.c_str());
+    rename(tmpname.c_str(), filename.c_str());
+}
+
+
 int main() {
-    
-    
+    Human user;
+    add_notes(user);
+    cout << "Вы ввели:" << endl;
+    show_notes();
+    cout << "Содержание файла после преобразований:" << endl;
+    replace_notes(user);
+    show_notes();
+
     return 0;
 }
