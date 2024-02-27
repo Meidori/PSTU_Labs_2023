d84e1d3b1d1a529b7cb752b4d74195d4439b7d8c
Meidori <meidorislav@gmail.com>
2024-02-27 13:03:47 +0500

    Del wrong solution in 8.cpp

--- Sem_2/Labs/8_ЗЕЛ/8.cpp
+++ Sem_2/Labs/8_ЗЕЛ/8.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <string>
-#include <stdio.h>
+#include <fstream>
 using namespace std;
 
 
@@ -15,37 +15,8 @@ struct Human {
 };
 
 
-void add_note(Human &user) {
-
-    cout << "Введите ФИО (не более 100 символов), год рождения, рост, вес:" << endl;
-    scanf("%s", user.full_name);
-    scanf("%d", &user.year_of_birth);
-    scanf("%f", &user.height);
-    scanf("%f", &user.weight);
-
-    fwrite(&user, sizeof(Human), 1, f);
-}
-
-
-void del_note(Human user) {
-
-}
-
-
-void show_notes(Human user) {
-    
-}
-
-
 int main() {
-    Human user;
-    f = fopen("file.dat", "wb");
-    add_note(user);
-    fclose(f);
-
-    f = fopen("file.dat", "rb");
-    show_notes(user);
-
-
+    
+    
     return 0;
 }
