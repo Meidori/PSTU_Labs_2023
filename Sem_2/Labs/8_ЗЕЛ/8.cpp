c087300a4e3e6c5520424db0afcf8d7a3f923af7
Meidori <meidorislav@gmail.com>
2024-03-09 15:25:37 +0500

    add a function for writing to a file

--- Sem_2/Labs/8_ЗЕЛ/8.cpp
+++ Sem_2/Labs/8_ЗЕЛ/8.cpp
@@ -4,69 +4,79 @@
 using namespace std;
 
 
-FILE *f;
-
-
 struct Human {
     string full_name;
     int year_of_birth;
-    string height;
-    string weight;
+    float height;
+    float weight;
 };
 
 
-void add_notes(Human &user) {
-    ofstream file("file.txt", ios::binary);
-
-    cout << "Введите ФИО, год рождения, рос, вес:" << endl;
-    getline(cin, user.full_name);
-    cin >> user.year_of_birth;
-    cin >> user.height;
-    cin >> user.weight;
-
-    file << user.full_name << "\n";
-    file << user.year_of_birth << "\n";    
-    file << user.height << "\n";    
-    file << user.weight << "\n";    
+void create_note(Human human, ostream &file) {
+    cout << "Введите ФИО, год рождения, рост, вес: " << endl;
     
-    file.close();
+    getline(cin, human.full_name);
+    int size = human.full_name.length();
+    file.write((char*)&size, sizeof(int));
+    file.write(human.full_name.c_str(), size);
+
+    cin >> human.year_of_birth;
+    file.write((char*)&human.year_of_birth, sizeof(int));
+    
+    cin >> human.height;
+    file.write((char*)&human.height, sizeof(float));
+    
+    cin >> human.weight;
+    file.write((char*)&human.weight, sizeof(float));
 }
 
 
-void show_notes() {
-    ifstream file("file.txt", ios::binary);
-    string line;
-    while (getline(file, line)) {
-        cout << line << endl;
-    }
+void get_note(Human human, ifstream &file) {
+    string name;
+    
+    int year;
+    float hght, wght;
+    
+    int size;
+    file.read((char*) &size, sizeof(int));
+    char buffer[size];
+    file.read(buffer, size);
+    name = string(buffer); 
 
-    file.close();
-}
-
-
-void replace_notes(Human &user) {
-    string filename = "file.txt";
-    string tmpname = "tmp.txt";
-    ifstream file(filename, ios::binary);
-    ofstream tmp(tmpname, ios::binary);   
-        
-
-    file.close();
-    tmp.close();
-
-    remove(filename.c_str());
-    rename(tmpname.c_str(), filename.c_str());
+    file.read((char*)&year, sizeof(int));
+    
+    file.read((char*)&hght, sizeof(float));
+    
+    file.read((char*)&wght, sizeof(float));
+    
+    cout << name << endl;
+    cout << year << endl;
+    cout << hght << endl;
+    cout << wght << endl;
 }
 
 
 int main() {
-    Human user;
-    add_notes(user);
-    cout << "Вы ввели:" << endl;
-    show_notes();
-    cout << "Содержание файла после преобразований:" << endl;
-    replace_notes(user);
-    show_notes();
+    Human human;
+    ofstream fo("file.bin", ios::binary);
+    if (fo.is_open()) {
+        create_note(human, fo);
+        fo.close();
+    }
+    else {
+        cout << "Не удалось открыть файл для записи." << endl;
+    }
+
+    ifstream fi("file.bin", ios::binary);
+    if (fi.is_open()) {
+        cout << "Вы ввели: " << endl;
+        get_note(human, fi);
+        fi.close();
+    }
+    else {
+        cout << "Не удалось открыть файл для чтения" << endl;
+    }
 
     return 0;
 }
+
