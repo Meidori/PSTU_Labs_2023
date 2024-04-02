a6ec5e474c2142cb7a55ef2c0ecfc9b4e34023ce
Meidori <meidorislav@gmail.com>
2024-04-02 12:38:57 +0500

    Code 20.cpp

--- Sem_2/Labs/20/equation.h
+++ Sem_2/Labs/20/equation.h
@@ -1,3 +1,57 @@
 #pragma once
 
 
+class Equation {
+private:
+    double coefficient_A;
+    double coefficient_B;
+    double coefficient_C;
+
+public:
+    Equation(double A = 0, double B = 0, double C = 0) {
+        coefficient_A = A;
+        coefficient_B = B;
+        coefficient_C = C;
+    }
+
+
+    ~Equation() {
+        std::cout << "Уравнение " << coefficient_A << "x^2 + " << coefficient_B << "x + " << coefficient_C << std::endl;
+        std::cout << "Удалено из памяти." << std::endl;
+    }
+
+
+    void show() {
+        std::cout << coefficient_A << "x^2 + " << coefficient_B << "x + " << coefficient_C << std::endl;
+    }
+
+
+    void set_A(double A) {
+        coefficient_A = A;
+    }
+
+
+    void set_B(double B) {
+        coefficient_B = B;
+    }
+
+
+    void set_C(double C) {
+        coefficient_C = C;
+    }
+
+
+    void get_A() {
+        std::cout << coefficient_A << std::endl;
+    }
+
+
+    void get_B() {
+        std::cout << coefficient_B << std::endl;
+    }
+
+
+    void get_C() {
+        std::cout << coefficient_C << std::endl;
+    }
+};
