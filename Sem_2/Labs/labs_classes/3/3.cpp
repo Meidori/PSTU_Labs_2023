32e2a4c3ba44aa2c2609d49599c8f49c86d38d81
Meidori <meidorislav@gmail.com>
2024-04-08 17:19:31 +0500

    Lab 3

--- Sem_2/Labs/labs_classes/3/3.cpp
+++ Sem_2/Labs/labs_classes/3/3.cpp
@@ -1,8 +1,30 @@
 #include <iostream>
+#include "time.h"
 
 
 int main() {
+    Time time1;
+    std::cout << "Введите минуты и секунды для объекта time1:" << std::endl;
+    std::cin >> time1;
+    std::cout << "time1:" << time1 << std:: endl;
+
+    Time time2 = time1;
+    std::cout << "Создан объект time2 и ему присвоено значение time1." << std::endl;
     
+    int subtraction_seconds;
+    std::cout << "Введите количество секунд, которое будет вычтено из time2:" << std::endl;
+    std::cin >> subtraction_seconds;
+    time2 = time2 - subtraction_seconds;
+    std::cout << "time2:" << time2 << std:: endl;
+
+    bool comparison;
+    std::cout << "Результат операции сравнения time1 == time2:" << std::endl;
+    comparison = time1 == time2;
+    std::cout << comparison << std::endl;
+    
+    std::cout << "Результат операции сравнения time1 != time2:" << std::endl;
+    comparison = time1 != time2;
+    std::cout << comparison << std::endl;
 
     return 0;
 }
--- Sem_2/Labs/labs_classes/3/time.h
+++ Sem_2/Labs/labs_classes/3/time.h
@@ -7,8 +7,91 @@ private:
     int seconds;
 
 public:
-    Time(int mins, int secs) {
+    Time(int mins = 0, int secs = 0) {
         minutes = mins;
         seconds = secs;
+        if (seconds >= 60) {
+            int extra_mins = seconds / 60;
+            seconds -= extra_mins * 60;
+            minutes += extra_mins;
+        }
+    }
+
+
+    int get_mins() {
+        return minutes;
+    }
+
+
+    int get_secs() {
+        return seconds;
+    }
+
+
+    void set_mins(int mins) {
+        minutes = mins;
+    }
+
+
+    void set_secs(int secs) {
+        seconds = secs;
+    }
+
+
+    Time operator =(const Time& time) {
+        if (&time == this) {
+            return *this;
+        }
+        minutes = time.minutes;
+        seconds = time.seconds;
+        return *this;
+    }
+
+
+    Time operator -(const int secs) {
+        seconds += minutes * 60;
+        seconds -= secs;
+        minutes = seconds / 60;
+        seconds -= minutes * 60;
+
+        return Time(minutes, seconds);
+    }
+
+
+    Time operator +(const int secs) {
+        seconds += secs;
+        int extra_mins = seconds / 60;
+        minutes += extra_mins;
+        seconds -= extra_mins * 60; 
+
+        return Time(minutes, seconds);
+    }
+
+
+    bool operator ==(const Time& time) const {
+        return (seconds == time.seconds) && (minutes == time.minutes);
+    }
+
+
+    bool operator !=(const Time& time) const {
+        return (seconds != time.seconds) && (minutes != time.minutes);
     }
 };
+
+
+std::ostream& operator <<(std::ostream& out, Time time) {
+    out << time.get_mins() <<  ":" << time.get_secs();
+    return out;
+}
+
+
+std::istream& operator >>(std::istream& in, Time& time) {
+    int mins, secs;
+    in >> mins;
+    in >> secs;
+    mins += secs / 60;
+    secs -= secs / 60 * 60;
+    time.set_mins(mins);
+    time.set_secs(secs);
+    return in;
+}
